a2aba6a3af6e6011ebc2782421e9d168df76bc5b
Max Siz <maxsizmobile@gmail.com>
2024-07-25 19:43:25 +0700

    FEAT Onchain Mltisig Model with time from  signers will able  to sign

---
+++ src/DeTrustMultisigOnchainModel_00.sol
@@ -0,0 +1,40 @@
+// SPDX-Licence-Identifier: MIT
+// UBD Network DeTrustMultisigOnchainModel_00
+pragma solidity 0.8.26;
+
+import "./MultisigOnchainBase_01.sol";
+import "./FeeManager_01.sol";
+
+/**
+ * @dev This is a  trust model onchain multisig implementation.
+ * Upon creation the addresses of the heirs(co-signers) and 
+ * the time (T) from each cosigner can sign transactios. 
+ * 
+ * !!! This is implementation contract for proxy conatract creation
+ */
+contract DeTrustMultisigOnchainModel_00 is MultisigOnchainBase_01, FeeManager_01 {
+
+    /////////////////////////////////////////////////////
+    /// OpenZepelin Pattern for Proxy initialize      ///
+    /////////////////////////////////////////////////////
+    function initialize(
+        uint8 _threshold,
+        address[] calldata _cosignersAddresses,
+        uint64[] calldata _validFrom,
+        address _feeToken,
+        uint256 _feeAmount,
+        address _feeBeneficiary,
+        uint64 _feePrepaidPeriod
+       
+    ) public initializer
+    {
+        __MultisigOnchainBase_01_init(
+            _threshold, _cosignersAddresses, _validFrom
+        );
+
+        __FeeManager_01_init(
+            _feeToken, _feeAmount, _feeBeneficiary, _feePrepaidPeriod
+        );
+    }
+
+}
