fb7eb5dbc4695a9f6298f0b3757a30df6d72f3d5
Alex <alexamuek@gmail.com>
2024-08-07 09:00:25 +0800

    fix error in remove function

--- src/MultisigOnchainBase_01.sol
+++ src/MultisigOnchainBase_01.sol
@@ -234,13 +234,15 @@ abstract contract MultisigOnchainBase_01 is
     {
         MultisigOnchainBase_01_Storage storage $ = _getMultisigOnchainBase_01_Storage();
         // decrease count for succesfull tx (GAS SAFE)
+        //signersCount = uint8($.cosigners.length - 1);
         signersCount = uint8($.cosigners.length - 1);
         require(signersCount >= $.threshold, "New Signers count less then threshold");
-        require(_signerIndex == 0, "Cant remove multisig owner(creator)");
+        require(_signerIndex != 0, "Cant remove multisig owner(creator)");
         emit SignerRemoved($.cosigners[_signerIndex], signersCount);
         // if deleting index is not last array element then need to replace it with last
         if (_signerIndex != signersCount + 1) {
-            $.cosigners[_signerIndex] = $.cosigners[signersCount + 1];
+            //$.cosigners[_signerIndex] = $.cosigners[signersCount + 1];
+            $.cosigners[_signerIndex] = $.cosigners[signersCount];
         }
         $.cosigners.pop();
     }
